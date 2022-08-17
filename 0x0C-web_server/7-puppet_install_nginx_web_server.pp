a6186ddc8b99099e28afb0bc8272366ab21c30c9
Alex <asenges400@gmail.com>
2022-08-16 22:17:36 -0300

    0x0C. Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,20 @@
-# Install nginx listening on port 80
+# Puppet Manifest to install nginx listening on port 80
 
 package { 'nginx':
   ensure => installed,
   name   => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+  path    => '/var/www/html/index.html'
+}
+
+file_line { 'title':
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
 }
 
 service { 'nginx':
