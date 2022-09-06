c1c651b1567549892b065d4a4b2efecd7df66ace
Alex <asenges400@gmail.com>
2022-09-06 18:42:26 -0300

    0x15. API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+API connect
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Main
+    """
+    if len(argv) > 1:
+        try:
+            uid = int(argv[1])
+            url = "https://jsonplaceholder.typicode.com/users/"
+            userRequest = requests.get("{}/{}".format(url, uid))
+            data = userRequest.json()
+            if userRequest.json().get("name") is not None:
+                todoListRequest = requests.get("{}{}/todos".format(url, uid))
+                todoList = todoListRequest.json()
+                taskCounter = len(todoList)
+                doneTasks = []
+                for thisTask in todoList:
+                    if thisTask.get("completed") is True:
+                        doneTasks.append(thisTask)
+                doneTaskCount = len(doneTasks)
+                print("Employee {} is done with tasks({}/{}):"
+                      .format(userRequest.json().get("name"), doneTaskCount, taskCounter))
+                for thisTask in doneTasks:
+                    print("\t {}".format(thisTask.get("title")))
+            else:
+                print("User doesn't exists")
+        except ValueError:
+            print("Please enter the user Id number")
+    else:
+        print("Please enter the user Id number")
