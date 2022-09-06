629f7b83373638043769f2ef504dd05599bb6845
Alex <asenges400@gmail.com>
2022-09-06 19:48:44 -0300

    0x15. API

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+3. Dictionary of list of dictionaries
+"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """
+    Main
+    """
+    if len(argv) > 1:
+        try:
+            uid = int(argv[1])
+            url = "https://jsonplaceholder.typicode.com/users/"
+            userRequest = requests.get("{}/{}".format(url, uid))
+            data = userRequest.json()
+            if userRequest.json().get("username") is not None:
+                todoListRequest = requests.get("{}{}/todos".format(url, uid))
+                with open("{}.json".format(uid), 'w', newline='') as f:
+                    obj = []
+                    for task in todoListRequest.json():
+                        obj.append({'username': userRequest.json()
+                                    .get('username'),
+                                    'task': task.get('title'),
+                                    'completed': task.get('completed')})
+                    file = {'{}'.format(uid): obj}
+                    json.dump(file, f)
+            else:
+                print("User doesn't exists")
+        except ValueError:
+            print("Please enter the user Id number")
+    else:
+        print("Please enter the user Id number")
